7e50576710fa57493e06c7665f0e61b8ac325df1
srivasd <srivasdelgado@gmail.com>
2018-05-30 21:19:31 +0200

    Version 1.1.5

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,6 @@
 import './index.css';
 import OpenviduHangoutsReact from './components/OpenviduHangoutsReact';
 import 'typeface-roboto';
-import 'material-design-icons';
 
 export default OpenviduHangoutsReact;
 
